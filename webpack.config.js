1929ea877e15587c5dce6bffd197e28bc6f11ef3
yangshuaiWithCompany <go.xlv@qq.com>
2017-11-02 19:26:07 +0800

    项目初始化

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const buildPath = __dirname + '/src/';
+
+const config = {
+    devtool: 'eval-source-map',    //生成Source Maps,这里选择eval-source-map
+    entry: {
+        login: buildPath + 'login.js',
+        main: buildPath + 'main.js'
+    },
+    output: {
+        filename: '[name].js',
+        path: __dirname + '/app/public'
+    },
+    module: {
+        rules: [
+          {
+            test: /\.js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['env','react']
+              }
+            }
+          },
+          {
+            test: /\.css$/,
+            use: [
+              {loader: "style-loader",options: { url: false, minimize: true }},
+              {loader: "css-loader",options: { url: false, minimize: true }}
+            ]
+          },
+          {
+            test: /\.(png|jpg|gif)$/,
+            use: [
+              {
+                loader: 'url-loader',
+                options: {
+                  limit: 8192,
+                  name: '[name].[ext]',
+                  outputPath: 'images/'
+                }
+              }
+            ]
+          }
+        ]
+      }
+};
+
+module.exports = config;
